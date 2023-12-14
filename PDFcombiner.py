e5afa52fc19271a4c7f6677f097ab2c4d114cd06
zEleceed <133176949+zEleceed@users.noreply.github.com>
2023-12-14 15:53:29 +0900

    Update PDFcombiner.py
    Adjusted code to be able to select wanted pdf files!

--- PDFcombiner.py
+++ PDFcombiner.py
@@ -1,22 +1,43 @@
 import PyPDF2
 import os
 
-pdfFiles = []
-pdfDirectory = input("Input PDF Directory: ")
+pdfFiles = {}
+pdfDirectory = input("PDF directory ")
 directory = pdfDirectory
 os.chdir(directory)
 
+counter = 1
 for filename in os.listdir(directory):
     if filename.endswith(".pdf"):
-        pdfFiles.append(filename)
+        pdfFiles[counter] = filename
+        counter += 1
 
-for file in pdfFiles:
-    print(f'\t{file}')
+for counter, file in pdfFiles.items():
+    print(f'\t{counter}: {file}')
 
 pdfWriter = PyPDF2.PdfWriter()
-pdfFiles = sorted(pdfFiles, key=str.lower)
 
-for filename in pdfFiles:
+chosenFiles = []
+print(f"Choose which files you want combined")
+print(f"If all of them type 'all'. If you are finished, press 0")
+while True:
+    inputChoice = input(f'Please Choose file or an option: ')
+    if inputChoice == "0":
+        break
+    elif inputChoice == "all":
+        chosenFiles = list(pdfFiles.values())  # directly assign all values
+        break
+    else:
+        try:
+            choice = int(inputChoice)
+            if choice in pdfFiles:  # Check if choice is a key in pdfFiles
+                chosenFiles.append(pdfFiles[choice])
+            else:
+                print("Invalid choice, please try again.")
+        except ValueError:
+            print("Invalid input, please enter a number.")
+
+for filename in chosenFiles:
     pdfReader = PyPDF2.PdfReader(filename)
     for pageNum in range(len(pdfReader.pages)):
         pageObj = pdfReader.pages[pageNum]
